659bdcb9a62404bd186b716d61393b5999f182cf
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-06-01 22:30:13 -0300

    comienzo ej6

---
+++ Programacion3-3/src/prog3/grafos/utiles/Ejercicio6.java
@@ -0,0 +1,50 @@
+package prog3.grafos.utiles;
+import prog3.grafos.*;
+import prog3.listagenerica.*;
+import prog3.colagenerica.*;
+
+public class Ejercicio6 {
+	private Grafo<String> grafo = new Grafo<String>();
+	
+	public ListaGenerica<String> devolverCamino(String ciudad1, String ciudad2) {
+	    ListaGenerica<String> camino = new ListaGenericaEnlazada<>();
+
+	    // Verificar si ciudad1 y ciudad2 existen en el grafo de ciudades
+	    if (!grafo.existeVertice(ciudad1) || !grafo.existeVertice(ciudad2)) {
+	        return camino; // Retorna lista vacía si alguna ciudad no existe
+	    }
+
+	    // Realizar un recorrido en profundidad (DFS) desde ciudad1 hasta encontrar ciudad2
+	    boolean[] visitado = new boolean[grafo.listaDeVertices().tamanio()];
+	    dfs(ciudad1, ciudad2, camino, visitado);
+
+	    return camino;
+	}
+
+	private void dfs(String ciudadActual, String ciudadDestino, ListaGenerica<String> camino, boolean[] visitado) {
+	    visitado[grafo.posicion(ciudadActual)] = true;
+	    camino.agregarFinal(ciudadActual);
+
+	    if (ciudadActual.equals(ciudadDestino)) {
+	        return; // Se llegó a la ciudad destino, se termina el recorrido
+	    }
+
+	    ListaGenerica<Arista<String>> adyacentes = grafo.listaDeAdyacentes(ciudadActual);
+	    adyacentes.comenzar();
+	    while (!adyacentes.fin()) {
+	        String ciudadAdyacente = adyacentes.proximo().verticeDestino();
+	        if (!visitado[grafo.posicion(ciudadAdyacente)]) {
+	            dfs(ciudadAdyacente, ciudadDestino, camino, visitado);
+	            if (!camino.estaVacia() && camino.elemento(camino.tamanio() - 1).equals(ciudadDestino)) {
+	                return; // Se encontró el camino, se termina el recorrido
+	            }
+	        }
+	    }
+
+	    // Si no se encontró el camino, se retrocede y se elimina la ciudad actual del camino
+	    if (!camino.estaVacia() && !camino.elemento(camino.tamanio() - 1).equals(ciudadDestino)) {
+	        camino.eliminarEn(camino.tamanio());
+	    }
+	}
+
+}
--- Programacion3-3/src/prog3/grafos/utiles/Recorridos.java
+++ Programacion3-3/src/prog3/grafos/utiles/Recorridos.java
@@ -1,5 +1,59 @@
 package prog3.grafos.utiles;
+import prog3.grafos.*;
+import prog3.listagenerica.*;
+import prog3.colagenerica.*;
 
-public class Recorridos {
+public class Recorridos<T> {
+public void dfs(Grafo<T> grafo){
+	boolean[] marca = new boolean[grafo.listaDeVertices().tamanio()]; 
+	for (int i=0; i<=marca.length;i++) {
+	      if (!marca[i])
+	    	  this.dfs(i, grafo, marca);
+	}
+}
+private void dfs(int i, Grafo<T> grafo, boolean[] marca){ 
+	marca[i] = true;
+	Vertice<T> v = grafo.listaDeVertices().elemento(i); 
+	System.out.println(v);
+	ListaGenerica<Arista<T>> adyacentes = grafo.listaDeAdyacentes(v);
+	adyacentes.comenzar();
+	while(!adyacentes.fin()){
+	      int j = adyacentes.proximo().verticeDestino().posicion();
+	      if(!marca[j]){
+	    	  this.dfs(j, grafo, marca);
+	      }
+	}
+}
+
+public void bfs(Grafo<T> grafo) {
+	boolean[] marca = new boolean[grafo.listaDeVertices().tamanio()];
+	for (int i=0; i<marca.length; i++) {
+		if (!marca[i])
+	        this.bfs(i, grafo, marca);
+	} 
+}
+	private void bfs (int i, Grafo<T> grafo, boolean[] marca) {
+		ListaGenerica<Arista<T>> ady = null;
+		ColaGenerica<Vertice<T>> q = new ColaGenerica<Vertice<T>>();
+		q.encolar(grafo.listaDeVertices().elemento(i));
+		marca[i] = true;
+		while(!q.esVacia()) {
+			Vertice<T> v = q.desencolar();
+			System.out.println(v);
+			ady = grafo.listaDeAdyacentes(v);
+			ady.comenzar();
+			while(!ady.fin()) {
+				Arista<T> arista = ady.proximo();
+				int j = arista.verticeDestino().posicion();
+				if (!marca[j]) {
+					Vertice<T> w = arista.verticeDestino();
+					marca[j] = true;
+					q.encolar(w);
+				}
+			}
+			
+		}
+		
+	} 
 
 }
