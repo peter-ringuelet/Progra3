40fb24a6a50656931b05d3a3b6199a24f873b424
Pedro Ringuelet <peter.ringuelet@gmail.com>
2023-06-07 00:39:41 -0300

    parciales

--- Programacion3-3/src/prog3/grafos/utiles/Ciudad.java
+++ Programacion3-3/src/prog3/grafos/utiles/Ciudad.java
@@ -1,5 +1,27 @@
 package prog3.grafos.utiles;
 
 public class Ciudad {
+	private String nombre;
+	private int fase;
+	
+	public Ciudad(String nombre, int fase) {
+		this.nombre = nombre;
+		this.fase = fase;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public int getFase() {
+		return fase;
+	}
 
+	public void setFase(int fase) {
+		this.fase = fase;
+	}
 }
--- Programacion3-3/src/prog3/grafos/utiles/Parcial1.java
+++ Programacion3-3/src/prog3/grafos/utiles/Parcial1.java
@@ -1,5 +1,57 @@
 package prog3.grafos.utiles;
 
+import prog3.grafos.*;
+import prog3.listagenerica.*;
+
+
 public class Parcial1 {
+	
+	public ListaGenericaEnlazada<Ciudad> resolver (Grafo<Ciudad> ciudades, String origen, String destino){
+		boolean[] marca = new boolean [ciudades.listaDeVertices().tamanio() + 1];
+		ListaGenericaEnlazada<Ciudad> camino = new ListaGenericaEnlazada<Ciudad>();
+		
+		ListaGenerica<Vertice<Ciudad>> aux = ciudades.listaDeVertices();
+		boolean ok = false;
+		Vertice<Ciudad> v = null;
+		aux.comenzar();
+		while (!aux.fin() && !ok){
+			v = aux.proximo();
+			if (v.dato().getNombre().equals(origen)) {
+				ok = true;
+			}
+		}
+		if (ok && v.dato().getFase() != 1) {
+			resolver (ciudades,destino,marca,camino,v.posicion());
+		}
+		return camino;
+	}
+	
+	private boolean resolver(Grafo<Ciudad> ciudades, String destino, boolean []marca,ListaGenerica<Ciudad> camino, int i) {
+		boolean encontre = false;
+		marca[i] = true;
+		Vertice <Ciudad> v = ciudades.listaDeVertices().elemento(i);
+		camino.agregarFinal(v.dato());
+		if (v.dato().getNombre().equals(destino)) {
+			encontre = true;}
+		else {
+			ListaGenerica<Arista<Ciudad>> ady = ciudades.listaDeAdyacentes(v);
+			ady.comenzar();
+			while(!ady.fin() && !encontre) {
+				Arista<Ciudad> a = ady.proximo();
+				int j = a.verticeDestino().posicion();
+				if (!marca[j] && a.verticeDestino().dato().getFase() != 1) {
+					encontre = resolver (ciudades,destino,marca,camino,j);
+				}
+			}
+			if (!encontre) {
+				marca[i] = false;
+				camino.eliminarEn(camino.tamanio());
+			}
+			
+		}
+		
+		return encontre;
+	}
+	
 
 }
